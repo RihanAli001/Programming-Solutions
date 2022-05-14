69563fad16d3957343173e88ac72bc809f21721c
Parvej Ali <Rihanali01010@gmail.com>
2022-05-14 13:22:38 +0530

    Add one more file in C folder

---
+++ C++/Basic/01_Display_sum_of_two_numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	float a, b, sum;
+	cout<<"Enter value of A : ";
+	cin>>a;
+	cout<<"Enter value of B : ";
+	cin>>b;
+	sum = a + b;
+	cout<<"\nSum : "<<sum;
+}
