886c7ba1cee6a16156974ae9eb2a1f18af872717
Parvej Ali <Rihanali01010@gmail.com>
2022-05-14 13:08:30 +0530

    Add computer network folder and add one file of arc program

--- C/Basic/Maximum between two numbers.c
+++ C/Basic/Maximum between two numbers.c
@@ -1,16 +1,14 @@
-// 1. Find maximum between two numbers.
+// 18. Find maximum between two numbers.
 #include<stdio.h>
 int main()
 {
 	int a,b;
 	printf("Enter any two numbers : ");
 	scanf("%d%d",&a,&b);
-	if(a>b)
-	{
+	if(a>b){
 		printf("%d is the maximum number.",a);
 	}
-	else
-	{
+	else{
 		printf("%d is the maximum number.",b);
 	}
 }
